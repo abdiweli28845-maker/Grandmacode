6d3999a8cc30f5a0971d241930c10f11916714d7
abdiweli28845-maker <abdiweli.28845@students.kyu.ac.ke>
2025-09-24 18:03:08 +0300

    Add files via upload

---
+++ Units of SE.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+printf("1. Mathematics for science\n");
+printf("2. Discrete for Mathematics\n");
+printf("3. Computer systems and organisations\n");
+printf("4. Fundamentals of ICT\n");
+printf("5. Communication skills\n");
+printf("6. Introduction to programming\n");
+printf("7. Fundamentals of system Analysis and design");
+
+    return 0;
+}
